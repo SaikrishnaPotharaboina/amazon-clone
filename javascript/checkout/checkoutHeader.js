d034533384b4d9a914f515d2ed74e470c60c122e
Saikrishna <potharaboinasaikrishna1@gmail.com>
2025-11-10 11:34:14 +0530

    amazon to indexhtml file name changed

--- javascript/checkout/checkoutHeader.js
+++ javascript/checkout/checkoutHeader.js
@@ -9,14 +9,14 @@ export function renderCheckoutHeader(){
 const checkoutHeaderHTML = `
     <div class="header-content">
       <div class="checkout-header-left-section">
-        <a href="amazon.html">
+        <a href="index.html">
           <img class="amazon-logo" src="images/amazon-logo.png">
           <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
         </a>
       </div>
       <div class="checkout-header-middle-section">
         Checkout (<a class="return-to-home-link"
-         href="amazon.html">${cartQuantity} items</a>)
+         href="index.html">${cartQuantity} items</a>)
       </div>
       <div class="checkout-header-right-section">
         <img src="images/icons/checkout-lock-icon.png">
@@ -34,7 +34,7 @@ export function addProductrender(){
       document.querySelector('.js-order-summary').innerHTML = `
         <div class="empty-cart-simple">
           <p>Your cart is empty.</p>
-          <a href="amazon.html" class="view-products-btn">View products</a>
+          <a href="index.html" class="view-products-btn">View products</a>
         </div>
       `;
       renderCheckoutHeader();
